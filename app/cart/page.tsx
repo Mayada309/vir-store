f093003bcfeb01241bb0dc1003cdc56308abf04f
Mayada <mayadaelsayed383@gmail.com>
2024-10-21 18:31:37 +0300

    fix cart bug

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,11 +1,25 @@
 'use client';
+import { useState, useEffect } from 'react';
 
 function Cart() {
-  // if (localStorage.getItem('cart') === null) {
-  //   return <h2 className='max-w-7xl mx-auto text-3xl text-slate-700 tracking-wide text-center'>
-  //     Your cart is empty!
-  //     </h2>;
-  // }
+  const [isEmpty, setIsEmpty] = useState(true);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const cart = localStorage.getItem('cart');
+      if (cart !== null) {
+        setIsEmpty(false);
+      }
+    }
+  }, []);
+
+  if (isEmpty) {
+    return (
+      <h2 className='max-w-7xl mx-auto text-3xl text-slate-700 tracking-wide text-center'>
+        Your cart is empty!
+      </h2>
+    );
+  }
 
   return <div>Cart</div>;
 }
