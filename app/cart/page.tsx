33389dfa30b045b438ec0124dadca63575182a08
Mayada <mayadaelsayed383@gmail.com>
2024-11-02 16:38:44 +0200

    add checkout button to cart page

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -6,6 +6,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { clearCart, RootState } from '../store';
 import CartTotals from '@/components/cart/CartTotals';
 import { useState, useEffect } from 'react';
+import Link from 'next/link';
 
 function Cart() {
   const dispatch = useDispatch();
@@ -32,7 +33,7 @@ function Cart() {
   }
 
   return (
-    <div className='max-w-[700px] mt-8 flex flex-col mx-auto gap-4'>
+    <div className='max-w-[700px] mt-8 flex flex-col mx-auto gap-4 p-4'>
       {cartItems.map((product) => {
         return <CartItem key={product.id} product={product} />;
       })}
@@ -42,7 +43,15 @@ function Cart() {
       >
         clear cart
       </button>
-      <CartTotals />
+      <div className='flex justify-between items-center border-t'>
+        <CartTotals />
+        <Link
+          href='checkout'
+          className='capitalize bg-slate-700 text-white py-2 sm:px-4 p-2 rounded w-fit sm:text-lg text-xs'
+        >
+          checkout
+        </Link>
+      </div>
     </div>
   );
 }
